6b0881e8dccf94f53f28c11d3de5c9353d033ccb
Sana Suse <sana96@uw.edu>
2021-09-30 13:43:19 -0700

    Aggregate Dwell Time and Land Use

--- safe_graph_dwell.py
+++ safe_graph_dwell.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import json
 
-filename = "allHouston"
+filename = "allHouston" # allChicago, allHouston, allLA, allNY, allPh
 pdata = pd.read_csv("data/" + filename + ".csv")
-#pdata['date'] = pd.to_datetime(pdata["date_range_start"][0:10])
-#pdata.sort_values("date")
 
+# Create lists for each column
 # <5, 5-10, 11-20, 21-60, 61-120, 121-240, >240
 dict = {}
 dict["month"] = []
@@ -17,16 +16,16 @@ dict["61-120"] = []
 dict["121-240"] = []
 dict[">240"] = []
 
+# Iterate through entire dataframe
 for index, row in pdata.iterrows():
     month = row["date_range_start"]
-    #print(month)
-    month = str(month)[0:10]
-    if month == "NaT":
-        print("hi")
+    month = str(month)[0:10] # Grab the date portion of the timestamp
 
+    # Quick check to make sure it isn't the column header row
     if row["bucketed_dwell_times"] != "bucketed_dwell_times":
         arr = json.loads(row["bucketed_dwell_times"])
 
+        # Append data to lists inside of the dictionary
         dict["month"].append(month)
 
         dict["<5"].append(arr["<5"])
@@ -37,5 +36,6 @@ for index, row in pdata.iterrows():
         dict["121-240"].append(arr["121-240"])
         dict[">240"].append(arr[">240"])
 
+# Create a new dataframe from the dictionary and write a csv file
 resultdf = pd.DataFrame.from_dict(dict)
 resultdf.to_csv(path_or_buf="data/" + filename + "Dwell.csv", index=False)
